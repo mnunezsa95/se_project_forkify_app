04912acdfd322d9d107a3b1c796088076435889e
mnunezsa <mnunezsa95@gmail.com>
2023-09-28 10:35:33 -0400

    udpate

--- src/js/views/RecipeView.js
+++ src/js/views/RecipeView.js
@@ -1,8 +1,6 @@
 import View from "./View.js";
 
-// import icons from '../img/icons.svg'; // Parcel 1
 import icons from "url:../../img/icons.svg"; // Parcel 2
-import { Fraction } from "fractional";
 import fracty from "fracty";
 
 class RecipeView extends View {
